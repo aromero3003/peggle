bac6d12f6818d09231b66e5d3020c6878409040c
aromero3003 <alromero@fi.uba.ar>
2023-06-14 06:04:30 -0300

    chore: little help msg for the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+#
+# Function to display help information
+show_help() {
+    echo "Usage: build.sh [-r]"
+    echo "Options:"
+    echo "  --help, -h       Display this help message"
+    echo "  --rebuild, -r    Delete build and build from zero"
+}
+
+# Check all script arguments for help flag
+for arg in "$@"; do
+    if [[ "$arg" == "--help" || "$arg" == "-h" ]]; then
+        show_help
+        exit 0
+    fi
+done
 
 if [ $# -gt 1 ] || [ $1 != "-r" ] ; then
-    echo "error: usage: ./build.sh [-r]"
+    echo "Usage: ./build.sh [-r]"
     exit 1
 fi
 if [ $# -eq 1 ] && [ $1 = "-r" ]; then
