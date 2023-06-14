22810523b9cc17b3eb0f996c414a6bca3ccda071
aromero3003 <alromero@fi.uba.ar>
2023-06-14 19:53:51 -0300

    feat: bola desbloquea funciones

--- include/bola.h
+++ include/bola.h
@@ -4,10 +4,19 @@
 #include "poligono.h"
 struct bola_t {
     bola_t(float cx, float cy, float radio, int resolucion);
+    void actualizar(double dt);
+    void set_position(aVec2 p);
+
+    bool eyectar(float angle);
+
     bool dibujar(SDL_Renderer *renderer) const;
 
    private:
+    aVec2 centro;
+    aVec2 velocidad;
     poligono_t forma;
+    float radio;
+    bool cayendo;
 };
 
 #endif /* __BOLA_H__ */
--- source/bola.cpp
+++ source/bola.cpp
@@ -1,13 +1,37 @@
 #include "bola.h"
 
+#include "config.h"
 #include "vec2.h"
 
 //------------------------------------------ BOLA
 //------------------------------------------
 
+const aVec2 g(0, G);
+
 bola_t::bola_t(float cx, float cy, float radio, int resolucion)
-    : forma(radio, resolucion) {
-    forma.trasladar(aVec2(cx, cy));
+    : centro(cx, cy),
+      velocidad(aVec2Zero),
+      forma(radio, resolucion),
+      radio(radio),
+      cayendo(false) {
+    forma.trasladar(centro);
+}
+
+aVec2 computar_velocidad(aVec2 vi, aVec2 a, double dt) { return a * dt + vi; }
+
+aVec2 computar_posicion(aVec2 pi, aVec2 vi, double dt) { return vi * dt + pi; }
+
+bool bola_t::eyectar(float angle) {
+    if (cayendo) return false;
+    velocidad.set(BOLA_VI * std::cos(angle), BOLA_VI * std::sin(angle));
+    return true;
+}
+
+void bola_t::actualizar(double dt) {
+    if (not cayendo) return;
+    velocidad = ROZAMIENTO * computar_velocidad(velocidad, g, dt);
+    centro = computar_posicion(centro, velocidad, dt);
+    // checkear si la bola está estancada
 }
 
 bool bola_t::dibujar(SDL_Renderer *renderer) const {
