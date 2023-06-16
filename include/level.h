aab84eb665efd88e22b23596af63ec6c40029591
aromero3003 <alromero@fi.uba.ar>
2023-06-16 05:09:47 -0300

    feat: clase Level con obstaculos y puntaje en nivel

---
+++ include/level.h
@@ -0,0 +1,32 @@
+#ifndef __LEVEL_H__
+#define __LEVEL_H__
+
+#include <SDL_render.h>
+
+#include <vector>
+
+#include "bola.h"
+#include "obstaculo.h"
+
+class Level {
+   public:
+    Level(std::vector<obstaculo_t> &obstaculos);
+    Level(Level &&) = default;
+    Level(const Level &) = delete;
+    Level &operator=(Level &&) = default;
+    Level &operator=(const Level &) = delete;
+    ~Level() = default;
+
+    void handle_collisions(bola_t &bola);
+    void clean_touched_obstacles();
+    size_t update_score(const obstaculo_t &obs);
+    size_t update_multiplier();
+    void draw(SDL_Renderer *renderer);
+
+   private:
+    std::vector<obstaculo_t> obstaculos;
+    size_t naranjas, naranjas_golpeados;
+    size_t score, multiplier;
+};
+
+#endif /* __LEVEL_H__ */
---
+++ source/level.cpp
@@ -0,0 +1,76 @@
+#include "level.h"
+
+#include <SDL_render.h>
+
+#include "bola.h"
+#include "config.h"
+#include "obstaculo.h"
+#include "vec2.h"
+
+Level::Level(std::vector<obstaculo_t> &obstaculos)
+    : obstaculos(obstaculos), naranjas(0), naranjas_golpeados(0) {
+    for (const obstaculo_t &obs : obstaculos) naranjas += obs.es_naranja();
+}
+
+size_t Level::update_score(const obstaculo_t &obs) {
+    if (obs.get_tocado()) return 0;
+
+    size_t puntaje_azul = 10;
+    switch (obs.get_color()) {
+        case COLOR_AZUL:
+            // Las azules valen 20 puntos
+            break;
+
+        case COLOR_NARANJA:
+            puntaje_azul *= 10;
+            break;
+
+        case COLOR_VERDE:
+            puntaje_azul *= 50;  // como las moradas;
+            break;
+
+        case COLOR_GRIS:
+            puntaje_azul *= 0;
+            break;
+    }
+    // *puntos_totales = *puntos_totales + puntaje_azul * multiplicador;
+    return puntaje_azul * multiplier;
+}
+
+size_t Level::update_multiplier() {
+    if (naranjas_golpeados < MULT1) multiplier = 1;
+    if (naranjas_golpeados >= MULT1) multiplier = 2;
+    if (naranjas_golpeados >= MULT2) multiplier = 3;
+    if (naranjas_golpeados >= MULT3) multiplier = 5;
+    if (naranjas_golpeados >= MULT4) multiplier = 10;
+    return multiplier;
+}
+
+void Level::handle_collisions(bola_t &bola) {
+    for (auto &obs : obstaculos) {
+        if (obs.get_dibujar()) {
+            // obs.dibujar(renderer);
+            //
+            aVec2 norma(aVec2Zero);
+            if (obs.distancia(bola.position(), norma) < BOLA_RADIO) {
+                bola.reflejar(norma);
+                if (obs.es_gris()) continue;
+
+                naranjas_golpeados += obs.es_naranja() and obs.get_dibujar() and
+                                      not obs.get_tocado();
+                // puntaje_actualizar(&puntaje_en_nivel, obs, multiplicador);
+                obs.set_tocado(true);
+            }
+        }
+    }
+}
+
+void Level::clean_touched_obstacles() {
+    for (auto &obs : obstaculos)
+        if (obs.get_tocado()) obs.set_dibujar(false);
+}
+
+void Level::draw(SDL_Renderer *r) {
+    for (auto &obs : obstaculos)
+        if (obs.get_tocado()) obs.set_dibujar(false);
+}
