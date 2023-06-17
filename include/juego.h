b227340ba28e6e0222416366d258d49068c38fba
aromero3003 <alromero@fi.uba.ar>
2023-06-17 02:36:52 -0300

    feat: Game class to store Levels and score

--- include/juego.h
+++ include/juego.h
@@ -1,24 +1,11 @@
 #ifndef PEGGLE_H
 #define PEGGLE_H
 
-#include <SDL2/SDL.h>
+#include <cstddef>
+#include <vector>
 
-#include "obstaculo.h"
-#include "vec2.h"
-
-// ESTRUCTURAS de los tdas
-
-// struct bola;
-// typedef struct bola bola_t;
-
-// struct vidas;
-// typedef struct vidas vidas_t;
-
-// struct trayectoria;
-// typedef struct trayectoria trayectoria_t;
-
-// struct recuperador;
-// typedef struct recuperador recuperador_t;
+#include "level.h"
+#include "loader.h"
 
 typedef enum {
     GAME_CLOSE,
@@ -30,62 +17,27 @@ typedef enum {
 
 typedef size_t puntaje_t;
 
-// Funciones generales para la lógica del juego
+struct Game {
+   public:
+    Game(Loader &l);
+    Game(Game &&) = default;
+    Game(const Game &) = default;
+    Game &operator=(Game &&) = default;
+    Game &operator=(const Game &) = default;
+    ~Game() = default;
 
-// double computar_posicion(double pi, double vi, double dt);
-// double computar_velocidad(double vi, double a, double dt);
+    game_state_t getState() const;
+    size_t getTotalLevels() const;
+    size_t getCurrentLevelId() const;
+    Level *getCurrentLevel();
 
-// void reflejar(aVec2 norm, aVec2 &c, aVec2 &v);
+    bool levelUp();
+    bool levelDown();
 
-// Funciones de la bola
-
-// bola_t *bola_crear(float cx, float cy, float radio, int resolucion);
-// void bola_destruir(bola_t *bola);
-//
-// bool bola_dibujar(SDL_Renderer *renderer, bola_t *bola);
-
-// Funciones de la trayectoria
-
-// trayectoria_t *trayectoria_crear();
-// void trayectoria_destruir(trayectoria_t *tray);
-//
-// bool trayectoria_agregar_coordenada(trayectoria_t *tray, float x, float y);
-// bool trayectoria_dibujar(SDL_Renderer *renderer, trayectoria_t *tray);
-//
-// trayectoria_t *trayectoria_calcular(float xi, float yi, float vxi, float vyi,
-//                                     float a, float dt);
-
-// Funciones para las vidas
-
-// vidas_t *vidas_inicializar(size_t n, float cx, float cy);
-// void vidas_destruir(vidas_t *vidas);
-//
-// // void vidas_agregar(vidas_t *vidas);
-// void vidas_quitar(vidas_t *vidas);
-//
-// size_t vidas_restantes(vidas_t *vidas);
-// bool vidas_estan_agotadas(vidas_t *vidas);
-//
-// bool vidas_dibujar(SDL_Renderer *renderer, vidas_t *vidas);
-// void vidas_resetear(vidas_t *vidas);
-
-// Funciones para el recuperador de bolas
-
-// recuperador_t *recuperador_crear(float ancho, float alto, float velocidad);
-// void recuperador_destruir(recuperador_t *recuperador);
-//
-// void recuperador_mover(recuperador_t *recuperador, float dt);
-// void recuperador_dibujar(SDL_Renderer *renderer, recuperador_t *recuperador);
-//
-// bool recuperador_bola_recuperada(recuperador_t *recuperador, float cx,
-// float cy);
-
-// Funciones para manejar el puntaje
-
-// bool puntaje_dibujar_caja(float x, float y);
-void puntaje_actualizar(puntaje_t *puntos_totales, obstaculo_t &obstaculo,
-                        int multiplicador);
-void puntaje_actualizar_multiplicador(int *multiplicador,
-                                      size_t naranjas_golpeados);
+    // private:
+    game_state_t state;
+    std::vector<Level> levels;
+    size_t current_level;
+};
 
 #endif
--- source/juego.cpp
+++ source/juego.cpp
@@ -1,114 +1,30 @@
 #include "juego.h"
 
-#include <SDL2/SDL.h>
-#include <stdlib.h>
+#include "loader.h"
 
-#include "bola.h"
-#include "config.h"
-#include "obstaculo.h"
-#include "poligono.h"
-#include "tipos.h"
-#include "vec2.h"
-
-// ------------------------------------------ VIDAS
-// ------------------------------------------
-
-/*
-// Inicializa las vidas en la posicion cx cy
-vidas_t *vidas_inicializar(size_t n, float cx, float cy) {
-    vidas_t *vidas = (vidas_t *)malloc(sizeof(vidas_t));
-    if (vidas == NULL) return NULL;
-
-    vidas->figuras = (bola_t **)malloc(sizeof(bola_t) * n - 1);
-    if (vidas->figuras == NULL) {
-        free(vidas);
-        return NULL;
+Game::Game(Loader &l) : current_level(0), state(GAME_RUNNING) {
+    while (l.can_continue()) {
+        levels.emplace_back(l.read_level());
     }
-
-    for (int i = 0; i < n - 1; i++) {
-        vidas->figuras[i] = bola_t()
-            bola_crear(cx, cy + (BOLA_RADIO * 3 * i), BOLA_RADIO, BOLA_RESOL);
-        if (vidas->figuras[i] == NULL) {
-            for (size_t j = 0; j < i; j++) {
-                bola_destruir(vidas->figuras[j]);
-                free(vidas->figuras);
-                free(vidas);
-                return NULL;
-            }
-        }
-    }
-    vidas->totales = n;
-    vidas->dibujadas = n - 1;
-
-    return vidas;
 }
 
-void vidas_destruir(vidas_t *vidas) {
-    for (int i = 0; i < vidas->totales - 1; i++)
-        bola_destruir(vidas->figuras[i]);
-    free(vidas->figuras);
-    free(vidas);
+game_state_t Game::getState() const { return state; }
+
+size_t Game::getTotalLevels() const { return levels.size(); }
+
+size_t Game::getCurrentLevelId() const { return current_level; }
+
+Level *Game::getCurrentLevel() {  // debería ser const?
+    return &(levels[current_level]);
 }
 
-bool vidas_estan_agotadas(vidas_t *vidas) { return vidas->dibujadas == 0; }
-
-size_t vidas_restantes(vidas_t *vidas) { return 1 + vidas->dibujadas; }
-
-void vidas_resetear(vidas_t *vidas) { vidas->dibujadas = vidas->totales - 1; }
-
-void vidas_quitar(vidas_t *vidas) { (vidas->dibujadas)--; }
-
-void vidas_agregar(vidas_t *vidas) {
-    if(vidas->dibujadas < vidas->totales - 1)
-        (vidas->dibujadas)++;
-}
-
-bool vidas_dibujar(SDL_Renderer *renderer, vidas_t *vidas) {
-    if (vidas == NULL) return false;
-    for (size_t i = 0; i < vidas->dibujadas; i++) {
-        bola_dibujar(renderer, vidas->figuras[i]);
-    }
+bool Game::levelUp() {
+    if (current_level + 1 >= getTotalLevels()) return false;
+    current_level++;
     return true;
 }
-*/
-
-// -------------------------------------------- PUNTAJE
-// ----------------------------------------------------
-
-void puntaje_actualizar(puntaje_t *puntos_totales, obstaculo_t &obstaculo,
-                        int multiplicador) {
-    if (obstaculo.get_tocado()) return;
-
-    puntaje_t puntaje_azul = 10;
-    switch (obstaculo.get_color()) {
-        case COLOR_AZUL:
-            // Las azules valen 20 puntos
-            break;
-
-        case COLOR_NARANJA:
-            puntaje_azul *= 10;
-            break;
-
-        case COLOR_VERDE:
-            puntaje_azul *= 50;  // como las moradas;
-            break;
-
-        case COLOR_GRIS:
-            puntaje_azul *= 0;
-            break;
-    }
-    *puntos_totales = *puntos_totales + puntaje_azul * multiplicador;
-}
-
-void puntaje_actualizar_multiplicador(int *multiplicador,
-                                      size_t naranjas_golpeados) {
-    if (naranjas_golpeados < MULT1) (*multiplicador) = 1;
-
-    if (naranjas_golpeados >= MULT1) (*multiplicador) = 2;
-
-    if (naranjas_golpeados >= MULT2) (*multiplicador) = 3;
-
-    if (naranjas_golpeados >= MULT3) (*multiplicador) = 5;
-
-    if (naranjas_golpeados >= MULT4) (*multiplicador) = 10;
+bool Game::levelDown() {
+    if (current_level == 0) return false;
+    current_level--;
+    return true;
 }
